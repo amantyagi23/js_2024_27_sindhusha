c13a4be9a8b08900d1cc89af3e56c72f0321281c
Aman <119286791+amantyagi23@users.noreply.github.com>
2024-11-06 09:02:43 +0530

    Add files via upload

--- weatherApp/javascript/services/weatherService.js
+++ weatherApp/javascript/services/weatherService.js
@@ -7,11 +7,17 @@ const API_KEY = "8ff3b783b61261fc8bade7e97d3205fe"
 
 
 const WEATHERSERVICE = {
-    getDataFromBackend : async function(cityName){
+    getDataFromBackendByCityName : async function(cityName){
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
         const data = await response.json();
-        console.log(data)
-    }
+        return data
+    },
+    getDataFromBackendByLonAndLat: async function(lat,lon){
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+        const data = await response.json();
+        return data
+    },
+
 }
 
 export default WEATHERSERVICE;
